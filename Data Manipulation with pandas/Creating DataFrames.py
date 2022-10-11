a6f81b1729f2a36bfa0847acc8373e119548b6e3
KurtTsai0708 <115029069+KurtTsai0708@users.noreply.github.com>
2022-10-11 23:32:30 +0800

    Add files via upload

---
+++ Data Manipulation with pandas/Creating DataFrames.py
@@ -0,0 +1,23 @@
+#List of dictionaries
+
+avocados_list = [
+    {"date": "2019-11-03", "small_sold" : 10376832, "large_sold": 7835071},
+    {"date": "2019-11-10", "small_sold": 10717154, "large_sold": 8561348},
+]
+
+avocados_2019 = pd.DataFrame(avocados_list)
+
+print(avocados_2019)
+
+
+#Dictionary of lists
+
+avocados_dict = {
+  "date": ["2019-11-17","2019-12-01"],
+  "small_sold": [10859987,9291631],
+  "large_sold": [7674135,6238096]
+}
+
+avocados_2019 = pd.DataFrame(avocados_dict)
+
+print(avocados_2019)
