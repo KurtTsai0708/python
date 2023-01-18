005d5ee809feccc253644fd9381b310517c4ac11
KurtTsai0708 <115029069+KurtTsai0708@users.noreply.github.com>
2023-01-18 15:51:21 +0800

    Delete Joining Data with pandas directory

--- Joining Data with pandas/Merging a table to itself.py
+++
@@ -1,9 +0,0 @@
-#Self join
-
-crews_self_merged = crews.merge(crews, on='id', how='inner',
-                                suffixes=('_dir','_crew'))
-boolean_filter = ((crews_self_merged['job_dir'] == 'Director') & 
-                  (crews_self_merged['job_crew'] != 'Director'))
-direct_crews = crews_self_merged[boolean_filter]
-
-print(direct_crews.head())
