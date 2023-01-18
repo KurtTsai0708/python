7c2bd6f5dd13ff4c046fd149748886aca65f4b1b
KurtTsai0708 <115029069+KurtTsai0708@users.noreply.github.com>
2023-01-18 15:51:11 +0800

    Delete Introduction to Statistics in Python directory

--- Introduction to Statistics in Python/More probability distributions.py
+++
@@ -1,7 +0,0 @@
-#Modeling time between leads
-
-from scipy.stats import expon as ex
-
-print(ex.cdf(1, scale=2.5))
-print(1 - expon.cdf(4, scale=2.5))
-print(expon.cdf(4, scale=2.5) - expon.cdf(3, scale=2.5))
